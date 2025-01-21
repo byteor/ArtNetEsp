480c5c0bbb32e9df13caa4d18463f2e3679a0b5b
Andrey Vasilyev <sagegeek@gmail.com>
2025-01-20 22:26:27 -0600

    seeed xiao esp32s3

--- include/Version.h
+++ include/Version.h
@@ -1,9 +1,9 @@
 
     // AUTO GENERATED FILE, DO NOT EDIT
     #ifndef VERSION
-        #define VERSION "2025.1.3"
+        #define VERSION "2025.1.4"
     #endif
     #ifndef BUILD_TIMESTAMP
-        #define BUILD_TIMESTAMP "2025-01-06 20:15:45.216321"
+        #define BUILD_TIMESTAMP "2025-01-18 16:50:23.836843"
     #endif
     
