db77c47e9b37dbfb0ebecfbfba281e99c15478a5
Andrey Vasilyev <sagegeek@gmail.com>
2026-02-06 22:40:25 -0600

    servo update

--- include/Version.h
+++ include/Version.h
@@ -1,9 +1,9 @@
 
     // AUTO GENERATED FILE, DO NOT EDIT
     #ifndef VERSION
-        #define VERSION "2025.1.12"
+        #define VERSION "2025.1.26"
     #endif
     #ifndef BUILD_TIMESTAMP
-        #define BUILD_TIMESTAMP "2025-03-24 22:34:11.368575"
+        #define BUILD_TIMESTAMP "2025-11-22 16:37:06.006311"
     #endif
     
--- src/device/dmxServo.h
+++ src/device/dmxServo.h
@@ -34,6 +34,11 @@ DmxServo::DmxServo(uint8_t universe, uint8_t channel, uint8_t pin)
     this->pin = pin;
     this->state = 0;
     this->value = 90;
+
+#ifdef ESP32
+
+#endif
+
     Serial.print("Servo on pin: ");
     Serial.print(pin);
     Serial.print(" DMX channel: ");
@@ -47,51 +52,72 @@ uint8_t DmxServo::get(uint8_t channel)
     return data;
 }
 
+/*
+The goal is to translate DMX value [0...255] to a value acceptable by Servo
+which may depend on the servo model
+For FT90R it is 90 +- 20, i.e. [60...120]
+0, 255, 127 = stop
+0..63 - increasing ?clockwise?, 63 is the max speed
+64..127 - decreasing
+128..191 - increasing, 191 is the max speed
+192..255 - decreasing
+*/
+int getRotationalValue(uint8_t data)
+{
+    int dX = 0;
+    float dY = 30;
+    float d0 = 90;
+    float b = 0;
+    int section = 256 / 4;
+    float m = dY / (float)section;
+    if (data < section)
+    {
+        m *= -1;
+        b = d0;
+        dX = 0;
+    }
+    else if (data >= section * 3)
+    {
+        m *= -1;
+        b = d0 + dY;
+        dX = -section * 3;
+    }
+    else
+    {
+        dX = -section;
+        b = d0 - dY;
+    }
+    return (float)(data + dX) * m + b;
+}
+
+/*
+The goal is to translate DMX value [0...255] to a value acceptable by Servo
+MG90 is a 180 degree servo
+Having MIN, MAX, and OFFSET we can translate DMX value to a degree value even servo is 360 degrees
+*/
+int getAngleValue(uint8_t data)
+{
+    double MIN = 501, MAX = 2500; // 179;
+    double range = MAX - MIN;
+    double x = data;
+    return x * range / 255 + MIN;
+}
+
 void DmxServo::set(uint8_t dmxChannel, uint8_t data)
 {
-    /*
-    The goal is to translate DMX value [0...255] to a value acceptable by Servo
-    which may depend on the servo model
-    For FT90R it is 90 +- 20, i.e. [60...120]
-    0, 255, 127 = stop
-    0..63 - increasing ?clockwise?, 63 is the max speed
-    64..127 - decreasing
-    128..191 - increasing, 191 is the max speed
-    192..255 - decreasing
-    */
+
     if (dmxChannel == channel)
     {
-        this->data = data;
-        int dX = 0;
-        float dY = 30;
-        float d0 = 90;
-        float b = 0;
-        int section = 256 / 4;
-        float m = dY / (float)section;
-        if (data < section)
-        {
-            m *= -1;
-            b = d0;
-            dX = 0;
-        }
-        else if (data >= section * 3)
-        {
-            m *= -1;
-            b = d0 + dY;
-            dX = -section * 3;
-        }
-        else
-        {
-            dX = -section;
-            b = d0 - dY;
-        }
-        value = (float)(data + dX) * m + b;
-
+        value = getAngleValue(data);
         Serial.print("Servo: ");
         Serial.println(value);
         if (servo.attached())
         {
-            servo.write(value);
+            servo.writeMicroseconds(value);
+        }
+        else
+        {
+            Serial.print("Servo not attached");
         }
     }
 }
@@ -100,7 +126,7 @@ void DmxServo::start()
 {
     if (!servo.attached())
     {
-        servo.attach(pin);
+        servo.attach(pin, 500, 2500);
         state = 1;
         servo.write(value);
     }
