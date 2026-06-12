e12f24fafeb96389bae806f5f5f2bb02a24af3ed
Andrey Vasilyev <sagegeek@gmail.com>
2026-06-12 01:57:45 -0500

    chore: bump auto-generated build version
    v2026.1.27 -> v2026.1.28

--- include/Version.h
+++ include/Version.h
@@ -1,9 +1,9 @@
 
     // AUTO GENERATED FILE, DO NOT EDIT
     #ifndef VERSION
-        #define VERSION "2026.1.27"
+        #define VERSION "2026.1.28"
     #endif
     #ifndef BUILD_TIMESTAMP
-        #define BUILD_TIMESTAMP "2026-06-12 01:18:46.440640"
+        #define BUILD_TIMESTAMP "2026-06-12 01:53:41.002848"
     #endif
     
